8ae9a69c7c81a453d2fe5c45cd75dc32937549d6
KPVakhrushev <KPVakhrushev@gmail.com>
2023-10-19 15:33:07 +0400

    fix: изменить стиль кнопки лайка на странице сохраненных фильмов

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,19 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useLocation } from "react-router-dom";
 import './MovieCard.css';
-import Preloader from '../Preloader/Preloader';
 
 const  MoviesCard = ({movie, handleLike} ) => {
+  const location = useLocation();
   const saved = Boolean(movie.id);
   const durationMin =  movie.duration % 60;
   const durationHour = (movie.duration-durationMin) / 60;
   const duration = durationHour ? `${durationHour}ч ${durationMin}м`:`${durationMin}м`;
   const handleClick = () => window.open(movie.trailerLink, '_blank', 'noopener,noreferrer');
 
+  const saveButtonClass =  location.pathname.includes('saved')? 'movie-card__save_tmpl_remove' : (saved? 'movie-card__save_saved':'')
   return (
       <li className='movie-card'>
         <img src={movie.thumbnail} alt={movie.nameRU} className='movie-card__image' onClick={handleClick}/>
         <h2 className='movie-card__title'>{movie.nameRU}</h2>
-        <span className={'movie-card__save '+ (saved? 'movie-card__save_saved':'')} onClick={()=>handleLike(movie)} />
+        <span className={'movie-card__save '+saveButtonClass} onClick={()=>handleLike(movie)} />
         <span className="movie-card__duration">{duration}</span>
       </li>
   )
